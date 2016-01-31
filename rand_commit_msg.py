29271bc9f53c3aaddab029c89aac7942e7b337b8
ay27 <me@ay27.pw>
2016-01-31 15:10:23 +0800

    侍宴既过三爵，喧哗窃恐非仪。

---
+++ rand_commit_msg.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+
+import os
+import urllib.request
+import math
+import random
+from html.parser import HTMLParser
+import traceback
+
+# function getNewsc(){
+#     $("#suijisc img").attr("class","donghua");
+#     $.get("/chaxun/shicirand/?i="+Math.floor(Math.random()*10000+1), function (data, status) {
+#         if (status == 'success') {
+#             $("#suijiajax").html(data);
+#             $("#suijisc img").attr("class","");
+#         }
+#     });
+#     $("#suijiajax").load("/chaxun/shicirand/?i="+Math.floor(Math.random()*10000+1));
+# }
+import subprocess
+
+URL = 'http://www.shicimingju.com/chaxun/shicirand/?i='
+CMD_GIT_ADD_ALL = 'git add -A'
+CMD_GIT_COMMIT = 'git commit -m " %s " '
+CMD_GIT_PUSH = 'git push origin master'
+
+
+class ParseContent(HTMLParser):
+    def error(self, message):
+        print('error: %s' % message)
+
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.content = []
+        self.flag = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'div':
+            for (var, value) in attrs:
+                if var == 'class' and value == 'suijineirong':
+                    self.flag = True
+                    break
+
+    def handle_data(self, data):
+        if self.flag:
+            self.content.append(data)
+            return
+
+    def handle_endtag(self, tag):
+        if self.flag and tag == 'div':
+            self.flag = False
+
+
+def rand_msg():
+    shici_id = int(math.floor(random.random() * 10000 + 1))
+    g = urllib.request.urlopen(URL + str(shici_id))
+
+    content = g.read().decode('utf-8')
+    parser = ParseContent()
+    parser.feed(content)
+    return parser.content[random.randint(0, len(parser.content)-1)]
+
+
+def run_cmd(cmd):
+    print(cmd)
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    rc = p.returncode
+    if rc == 0:
+        return out
+    else:
+        raise AssertionError('\n%s' % err)
+
+
+if __name__ == '__main__':
+    try:
+        msg = rand_msg()
+        run_cmd(CMD_GIT_ADD_ALL)
+        run_cmd(CMD_GIT_COMMIT % msg)
+        run_cmd(CMD_GIT_PUSH)
+    except Exception as err:
+        print(traceback.format_exc())
+        exit(-1)
+    exit(0)
---
+++ readme.md
@@ -0,0 +1,9 @@
+### rand_commit_message
+随机扒取[诗词名句网](http://www.shicimingju.com/)的诗句,作为commit message.
+把常用的git add, git commit, git push三个命令写在一起,仅需一条命令即可把repo上传.
+
+### 环境:python3
+
+### 使用方法
+1. 安装: sudo ./INSTALL
+2. 使用: rgit
